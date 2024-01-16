7bef7f715b795e86ca030cce70c71ae8ea45bd17
RezkiBawono <rezkibawono@gmail.com>
2024-01-16 16:53:16 +0700

    refactoring

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -29,6 +29,7 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  revalidatePath('dashboard/invoices');
+
+  revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
